d41e47c764c9a0730daa1be4a169db98f415c852
AlexanderFl <AlexanderFl@LNZ-ALEXF.microfocus.com>
2013-04-24 20:12:01 +0200

    delete obsolete sample classes

--- app/Global.scala
+++ app/Global.scala
@@ -11,20 +11,6 @@ object Global extends GlobalSettings {
     TestCase.save(c)
     val c1 = TestCase(new ObjectId(), "test1", "class1j", "suite1", "Win 7 config", TestStatus("Passed"));
     TestCase.save(c1)
-//        if (User.count(DBObject(), Nil, Nil) == 0) {
-//      Logger.info("Loading Testdata")
-//      User.save(User(
-//        username = "leon",
-//        password = "1234",
-//        address = Some(Address("Örebro", "123 45", "Sweden"))
-//      ))
-//
-//      User.save(User(
-//        username = "guillaume",
-//        password = "1234",
-//        address = Some(Address("Paris", "75000", "France"))
-//      ))
-//    }
   }
 
   override def onStop(app: Application) {
