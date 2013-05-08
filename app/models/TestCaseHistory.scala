54fc572f7f57dc5c2ce5450e4bfe9ab89350062d
alexanderfloh <alexander.floh@gmail.com>
2013-05-08 06:46:04 +0200

    added experimental code for using MapReduceCommand

--- app/models/TestCaseHistory.scala
+++ app/models/TestCaseHistory.scala
@@ -13,6 +13,9 @@ import mongoContext._
 import com.mongodb.casbah.commons.MongoDBObject
 import org.joda.time.DateTime
 import play.libs.Json._
+import com.mongodb.casbah.map_reduce.MapReduceCommand
+import com.mongodb.casbah.map_reduce.MapReduceStandardOutput
+import com.mongodb.casbah.map_reduce.MapReduceInlineOutput
 
 case class TestCaseHistory(
   buildNumber: Int,
@@ -22,7 +25,7 @@ case class TestCaseHistory(
   comment: String,
   timestamp: DateTime,
   additionalData: Map[String, String]) {
-  
+
   def isDefect = additionalData.get("defect").map(_.toBoolean).getOrElse(false)
   def isCodeChange = additionalData.get("codeChange").map(_.toBoolean).getOrElse(false)
   def isTiming = additionalData.get("timing").map(_.toBoolean).getOrElse(false)
@@ -34,6 +37,79 @@ object TestCaseHistory extends ModelCompanion[TestCaseHistory, ObjectId] {
 
   def getHistoryByTestCase(testCase: TestCase): List[TestCaseHistory] = {
     dao.find(MongoDBObject("testName" -> testCase.testName, "className" -> testCase.className, "suiteName" -> testCase.suiteName)).sort(orderBy = MongoDBObject("timestamp" -> -1)).toList
+  }
+
+  val map = """function() {
+			var statusMapper = {
+				"Passed": "passed",
+				"Failed": "failed",
+				"Fixed": "passed",
+				"Regression": "failed"
+			};
+			var value = {
+				builds: {
+					passed: [],
+					failed: []
+				}
+			};
+			switch(statusMapper[this.status.name]) {
+				case "passed":
+					value.builds.passed.push(this.buildNumber);
+					break;
+				case "failed":
+					value.builds.failed.push(this.buildNumber);
+					break;
+			}
+			emit({
+				suite: this.suiteName,
+				clazz: this.className,
+				test: this.testName
+			}, value);
+		}"""
+
+  val reduce = """function(key, values) {
+			var reducedVal = {
+				builds: {
+					passed: [],
+					failed: []
+				}
+			};
+			values.forEach(function(val) {
+				val.builds.passed.forEach(function(build) {
+					if (reducedVal.builds.passed.indexOf(build) < 0) {
+						reducedVal.builds.passed.push(build);
+					}
+				});
+				val.builds.failed.forEach(function(build) {
+					if (reducedVal.builds.failed.indexOf(build) < 0) {
+						reducedVal.builds.failed.push(build);
+					}
+				});
+			});
+			return reducedVal;
+		}"""
+
+  val finalizeFunction = """function(key, reducedVal) {
+			var calcMax = function(arr) {
+				return arr.length ? Math.max.apply(Math, arr) : 0;
+			};
+			reducedVal.builds.latestPassed = calcMax(reducedVal.builds.passed);
+			reducedVal.builds.latestFailed = calcMax(reducedVal.builds.failed);
+			reducedVal.historyScore = reducedVal.builds.latestFailed - reducedVal.builds.latestPassed;
+			if (reducedVal.historyScore > 0) {
+				return reducedVal;
+			}
+			return null;
+		}"""
+
+  def calculateScore() = {
+    MapReduceCommand(
+      input = "testCases",
+      map = map,
+      reduce = reduce,
+      finalizeFunction = Some(finalizeFunction),
+      output = MapReduceInlineOutput)
+      .toDBObject
   }
 }
 
