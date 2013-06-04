338c7b8a026252576f07a582029b3298b3689947
Peter Donner <peter@markup.at>
2013-06-04 02:21:24 +0200

    Move class TestCaseKey into TestCase.scala.

--- app/models/TestCase.scala
+++ app/models/TestCase.scala
@@ -8,6 +8,14 @@ import play.api.data.format.Formats._
 import play.api.data.validation.Constraints._
 import reactivemongo.bson._
 
+case class TestCaseKey(suiteName: String, className: String, testName: String) {
+  def toUrlPart = {
+    val urlParts = List(suiteName, className, testName)
+    def encode(str: String) = java.net.URLEncoder.encode(str, "UTF-8")
+    urlParts.map(encode).mkString("/")
+  }
+}
+
 case class TestCaseConfiguration(
     name: String, 
     var passed: List[Int] = List(), 
@@ -79,9 +87,7 @@ case class TestCase(
   var configurations: List[TestCaseConfiguration] = List(),
   var feedback: List[TestCaseFeedback] = List(),
   score: Int = 10
-  ) {
-
-}
+  )
 
 object TestCase {
   implicit object TestCaseBSONReader extends BSONDocumentReader[TestCase] {
--- app/models/TestCaseKey.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-case class TestCaseKey(suiteName: String, className: String, testName: String) {
-  def toUrlPart = {
-    val urlParts = List(suiteName, className, testName)
-    def encode(str: String) = java.net.URLEncoder.encode(str, "UTF-8")
-    urlParts.map(encode).mkString("/")
-  }
-}
