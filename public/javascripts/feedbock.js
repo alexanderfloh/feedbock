629b8a78f86a5a4bd7a0968187253673efff7e87
alexanderfloh <alexander.floh@gmail.com>
2013-06-06 22:26:13 +0200

    merged new design

---
+++ public/javascripts/feedbock.js
@@ -0,0 +1,36 @@
+
+$(function() {
+
+	var _hide = function($element) {
+		$('body').removeClass('details-open');
+		if ($element) {
+			$element.removeClass('active');
+		}
+		$('#details').hide();
+	};
+
+	$('#details').on('click', '[data-action=close]', function () { _hide(); } );
+
+	$('.tests li').on('click', function () {
+		var $this = $(this);
+
+		// hide
+		if ($this.hasClass('active')) {
+			_hide($this);
+
+		// show
+		} else {
+			$('body').addClass('details-open');
+			$('.active').removeClass('active');
+			$this.addClass('active');
+			$('#details').html($this.find('.details').html());
+			var distanceToTop = $this.position().top - 100;
+			$('#details').show()
+				.css('margin-top', distanceToTop);
+		}
+	});
+
+
+
+});
+
